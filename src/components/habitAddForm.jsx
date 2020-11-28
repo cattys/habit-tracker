ae5a208ee861abdcd0bc4f590767c30b88998693
cattys <cattys0916@gmail.com>
2020-11-29 00:30:18 +0900

    React: study

--- src/components/habitAddForm.jsx
+++ src/components/habitAddForm.jsx
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
+import React, { memo } from 'react';
 
-class HabitAddForm extends Component {
-    inputRef = React.createRef();
+const HabitAddForm = memo((props) => {
+    const formRef = React.createRef();
+    const inputRef = React.createRef();
 
-    onSubmit = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault();
-        const name = this.inputRef.current.value;
-        name && this.props.onAdd(name);
-        this.inputRef.current.value ='';
-        // this.formRef.current.reset();
-        // ㄴ이거 잘안된다...
-        // this.inputRef.current.focus();
-    }
-
-    render() {
-        return (
-            <div>
-                <form ref={this.formRef} className="add-form" onSubmit={this.onSubmit}>
-                    <input ref={this.inputRef} type="text" className="add-input" placeholder="Habit"/>
-                    <button className="add-button">Add</button>
-                </form>
-            </div>
-        );
-    }
-}
+        const name = inputRef.current.value;
+        name && props.onAdd(name);
+        // inputRef.current.value ='';
+        formRef.current.reset();
+    };
+    return (
+        <form ref={formRef} className="add-form" onSubmit={onSubmit}>
+            <input ref={inputRef} type="text" className="add-input" placeholder="Habit"/>
+            <button className="add-button">Add</button>
+        </form>
+    );
+});
 
 export default HabitAddForm;
